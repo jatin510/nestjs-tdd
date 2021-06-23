0b5be4436527eba5af1be3e568f1e889bd620518
jatin_510 <jagdish@quor.com>
2021-06-23 13:49:59 +0530

    changed the findOne param

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,7 +13,7 @@ export class UsersService {
   }
 
   async findOne(id: string): Promise<User> {
-    return await this.userModel.findOne({ id });
+    return await this.userModel.findOne({ _id: id });
   }
 
   //   async create(): Promise<User> {
